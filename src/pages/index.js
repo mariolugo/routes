d857a507b84b43762af2dfa6bd1e9f8eb6978841
Mario Lugo Talamante <mariolugo23@gmail.com>
2020-12-16 10:55:07 -0600

    Installing initial libraries, precommiter, and eslint

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Routes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>Routes Project</p>
+      </main>
+    </div>
+  );
+}
